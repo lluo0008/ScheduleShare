12744554eb8043157195860640b15cc5f4607f0a
Erick <erickflo115@gmail.com>
2021-01-23 21:49:10 -0800

    added name functionality

--- public/front.js
+++ public/front.js
@@ -1,10 +1,25 @@
 function changeColor(obj_id) {
     var textIn = document.getElementById(obj_id).innerHTML;
-    if (textIn === "Open"){
-        document.getElementById(obj_id).style.backgroundColor = "#FFFFFF";
-        document.getElementById(obj_id).innerHTML = "Taken";
+    var name = document.getElementById("info").innerHTML;
+    if (name == "") {
+        alert("Please Enter a Name")
+        
     } else {
-        document.getElementById(obj_id).style.backgroundColor = "#DDDDDD";
-        document.getElementById(obj_id).innerHTML = "Open";
-    }                      
+        if (textIn === "Open"){
+            document.getElementById(obj_id).style.backgroundColor = "#FFFFFF";
+            document.getElementById(obj_id).innerHTML = name;
+            this.value=name;
+        } else {
+            document.getElementById(obj_id).style.backgroundColor = "#DDDDDD";
+            document.getElementById(obj_id).innerHTML = "Open";
+            this.value = "Open";
+        }   
+    }
+    //this.value="Lawrence";
+    //alert(this.value)
+}
+
+function dispInfo() {
+    var first = document.getElementById("fname").value;
+    document.getElementById("info").innerHTML = first;
 }
